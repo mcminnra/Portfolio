5a228fce9398dc5b29a62ea02aef2b637c27ac18
rmcminn <rmcminn@indiana.edu>
2015-01-05 19:54:26 -0500

    Updated Projects

--- js/controllers.js
+++ js/controllers.js
@@ -8,6 +8,69 @@ angular.module('myApp.controllers', ['ngSanitize'])
   });
 
 var projects = [
+	{
+		'classtags' : 'PROJECT HTML5CSS JAVASCRIPT PERSONAL',
+		"name" : "FitCalibrate",
+		"imgsrc" : "img/fitcalibrate.png",
+		"type" : 'Personal',
+		'languages' : [
+			{
+				'name':'HTML5'
+			},
+			{
+				'name':'CSS'
+			},
+			{
+				'name':'JavaScript'
+			}
+		],
+		'frameworks' : [
+			{
+				'name':'Node.js'
+			},
+			{
+				'name':'Jade'
+			},
+			{
+				'name':'Express.js'
+			},
+			{
+				'name':'D3.js'
+			},
+			{
+				'name':'BootStrap 3'
+			},
+			{
+				'name':'Mongo DB'
+			}
+		],
+		'description' : 'Developed a Node application using the MEN stack (Mongo DB, Express.js, Node.js) that is designed to track your fitness and display it through graphs. I utilize RedHat\'s OpenShift Cloud to deploy my application.',
+		'sourcecode' : '<a href="http://fitcalibrate-mcminnra.rhcloud.com/#">FitCalibrate</a>'
+	},
+	{
+		'classtags' : 'PROJECT HTML5CSS JAVASCRIPT PROFESSIONAL',
+		"name" : "DraftOnce",
+		"imgsrc" : "img/do_logo.png",
+		"type" : 'Professional',
+		'languages' : [
+			{
+				'name':'HTML5'
+			},
+			{
+				'name':'CSS'
+			},
+			{
+				'name':'JavaScript'
+			}
+		],
+		'frameworks' : [
+			{
+				'name':'Sencha Ext.js 5'
+			}
+		],
+		'description' : 'Worked on Blue Burro\'s DraftOnce updating the Front-End components. I was part of a team that was tasked with upgrading the application from Ext.js 3 to Ext.js 5.',
+		'sourcecode' : '<a href="http://www.draftonce.com">www.draftonce.com</a>'
+	},
     {
         'classtags' : 'PROJECT HTML5CSS JAVASCRIPT PROFESSIONAL',
         "name" : "www.beetownhive.com - Hive Nightclub",
@@ -151,41 +214,5 @@ var projects = [
 		],
 		'description' : 'My portfolio website that I use to display my projects and other assorted things.',
 		'sourcecode' : '<a href="https://www.github.com/rmcminn/Portfolio}"><i class="fa fa-github"></i> Portfolio</a>'
-	},
-	{
-		'classtags' : 'PROJECT JAVA PERSONAL',
-		"name" : "BitCoinInvestor",
-		"imgsrc" : "img/bitcoininvestor.png",
-		'type' : 'Personal',
-		'languages' : [
-			{
-				'name' : 'Java'
-			}
-		],
-		'frameworks' : [
-			{
-				'name' : 'Swing'
-			}
-		],
-		'description' : 'A simple program used to track bitcoin prices and saves them to a .csv',
-		'sourcecode' : '<a href="https://www.github.com/rmcminn/BitCoinInvestor"><i class="fa fa-github"></i> BitCoinInvestor</a>'
-	},
-	{
-		'classtags' : 'PROJECT CSHARP PERSONAL',
-		"name" : "VideoGameDatabaseManager",
-		"imgsrc" : "img/videogamedatabasemanager.png",
-		'type' : 'Personal',
-		'languages' : [
-			{
-				'name' : 'C#'
-			}
-		],
-		'frameworks' : [
-			{
-				'name' : '.NET'
-			}
-		],
-		'description' : 'A simple program used to manipulate an access database of video games',
-		'sourcecode' : '<a href="https://www.github.com/rmcminn/VideoGameDatabaseManager"><i class="fa fa-github"></i> VideoGameDatabaseManager</a>'
 	}
 ];
