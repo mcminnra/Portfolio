b235a196d64c4d425d10f54ff8548afa2d13c1cd
Ryder McMinn <mcminnra@gmail.com>
2018-05-29 01:29:02 -0400

    implemented flask

---
+++ portfolio/index.py
@@ -0,0 +1,40 @@
+import os
+
+from flask import Flask
+from flask import render_template, url_for
+
+app = Flask(__name__)
+application = app  # for Apache WSGI
+
+app.debug = "on"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/splash')
+def splash():
+    return render_template('splash.html')
+
+# Files
+@app.route('/humans.txt')
+def humans():
+    return app.send_static_file('humans.txt')
+
+@app.route('/robots.txt')
+def robots():
+    return app.send_static_file('robots.txt')
+
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
+
+# Errors
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='127.0.0.1', port=port)
