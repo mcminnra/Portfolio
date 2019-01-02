79afd06861c9334f349182987518f68f93fe7800
Ryder McMinn <mcminnra@ryders-mbp.attlocal.net>
2019-01-02 01:09:38 -0500

    simple logging

--- portfolio/index.py
+++ portfolio/index.py
@@ -1,28 +1,45 @@
+import logging
 import os
 
-from flask import Flask
-from flask import render_template, url_for
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 application = app  # for Apache WSGI
 
-app.debug = "on"
+app.debug = "off"
+
+# ...if being run on gunicorn
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+# Logging
+def log_access():
+    referer = request.environ.get('HTTP_REFERER')
+    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    user_agent = request.environ.get('HTTP_USER_AGENT')
+    app.logger.info(f'[{request.full_path}] IP: {ip}, Referer: {referer}, User-Agent: {user_agent}')   
 
 @app.route('/')
 def index():
+    log_access()
     return render_template('index.html')
 
 @app.route('/splash')
 def splash():
+    log_access()
     return render_template('splash.html')
 
 # Files
 @app.route('/humans.txt')
 def humans():
+    log_access()
     return app.send_static_file('humans.txt')
 
 @app.route('/robots.txt')
 def robots():
+    log_access()
     return app.send_static_file('robots.txt')
 
 @app.route('/favicon.ico')
@@ -32,6 +49,8 @@ def favicon():
 # Errors
 @app.errorhandler(404)
 def page_not_found(e):
+    log_access()
+    app.logger.error(f'404 Error - {request.full_path}')
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
